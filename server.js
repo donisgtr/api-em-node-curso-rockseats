e0be757800d1c33b09623739b13c9fcd45c74ab1
donissousa <donisgtr@gmail.com>
2020-08-21 01:29:43 -0300

    commit inicial

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
-
+const mongoose = require('mongoose')
 const app  = express();
+
+//mongoose.connect('mongodb2://localhost:27017/nodeapi' );
 
 app.get("/", (res, req) => {
     req.send("Hello World Donis");
 });
+
+
 
 app.listen(3001);
